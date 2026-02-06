71b31bf251054258a81d8e65ebb7a83799f2e73f
p-cap <slashedeye@gmail.com>
2026-02-05 23:50:07 -0600

    another day...another pythoning

---
+++ parking-lot.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        longest = 1
+        nums_set = set(nums)
+        for num in nums_set:
+            if (num - 1) not in nums_set:
+                length = 1
+                while num + length in nums_set:
+                    length += 1
+                longest = max(longest, length)
+        return longest 
